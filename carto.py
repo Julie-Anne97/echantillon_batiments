eb52df7aff7655b1e99830feae41289aaa3f4e0f
Julie-Anne <101122818+Julie-Anne97@users.noreply.github.com>
2022-11-09 08:46:48 +0100

    Update carto.py

--- carto.py
+++ carto.py
@@ -1,141 +1,70 @@
-import pandas as pd
 import streamlit as st
-import pydeck as pdk
-import folium
 from streamlit_folium import st_folium
+import folium
+import pandas as pd 
+from folium.plugins import MarkerCluster
 
 
-#import & clean data
-
+#import data
 df = pd.read_parquet("reducedfile.parquet.snappy", engine='pyarrow')
 
 pd.set_option('display.max_columns',None)
 df.head()
+
 df = df.rename(columns={'latitude' : 'lat' , 'longitude' : 'lon'})
 df = df.dropna(subset=['lat', 'lon'])
 df.info(verbose=True, null_counts=True)
 
-df = df.drop(df.iloc[:,72:94],axis=1)
+df = df[["lat","lon","siret", "siren", "id_ban","enseigne1Etablissement"]]
+df.shape
+df= df.drop_duplicates()
+df.shape
+
+df['entreprise'] = df[['siret', 'siren', 'id_ban',"enseigne1Etablissement"]].to_dict(orient='records')
+df
+
+
+#df = df[["SURF_HAB_TOTAL","lat","lon","geombui","adedpe202006_mean_class_estim_ges"]]
 df.reset_index()
-df.info(verbose=True, null_counts=True)
 
-df.adedpe202006_mean_class_estim_ges.unique()
-classedpe = list(df.adedpe202006_mean_class_estim_ges.unique())
 
-df = df.dropna(subset=['SURF_HAB_TOTAL'])
-#Streamlit UI
+#Add filter
 
 st.title('Test Master dataset')
 
 #Filtres
 
 dpe = ["A", "B","C", "D", "E", "F", "G", "N"]
-dpeselect = st.multiselect('Classe dpe :', classedpe,dpe)
+
 
 surfaces = st.slider('Sélectionner la surface',0.0,10000.00,(0.00,10000.00),step=200.00)
 
-df2 = df[df["adedpe202006_mean_class_estim_ges"].isin(dpeselect)]
-df3 =  df2[df2["SURF_HAB_TOTAL"].isin(surfaces)]
+# df2 = df[df["adedpe202006_mean_class_estim_ges"].isin(dpeselect)]
+# df3 =  df2[df2["SURF_HAB_TOTAL"].isin(surfaces)]
 
 
-#df3 = [df["SURF_HAB_TOTAL"].isin(surfaces)]
-#df4 = df[df["adedpe202006_mean_class_estim_ges"].isin(dpeselect)][df["SURF_HAB_TOTAL"].isin(surfaces)]
+bat_location = df[["lat","lon","entreprise"]]
 
-# st.map(df3, zoom=None, use_container_width=True)
-
-# st.write("Liste complète avec les filtres sélectionnées")
-# st.dataframe(df3)
-
-# def convert_df(df3):
-#     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-#     return df3.to_csv().encode('utf-8')
-
-# csv = convert_df(df3)
-
-# st.download_button(
-#     label="Télécharge moi",
-#     data=csv,
-#     file_name='large_df.csv',
-#     mime='text/csv')
-
-# center on Liberty Bell, add marker
-# m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-# folium.Marker(
-#     [39.949610, -75.150282], 
-#     popup="Liberty Bell", 
-#     tooltip="Liberty Bell"
-# ).add_to(m)
-
-# tile = folium.TileLayer(
-#         tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-#         attr = 'Esri',
-#         name = 'Esri Satellite',
-#         overlay = False,
-#         control = True
-#        ).add_to(m)
-
-# call to render Folium map in Streamlit
-# st_data = st_folium(m, width = 725)
-
-df3['lat'] = df3['lat'].astype(float)
-df3['lon'] = df3['lon'].astype(float)
-
-# df4 = df3[['lat', 'lon','libelleVoieEtablissement']].copy()
-
-latitude = list(df3['lat'])
-longitude = list(df3['lon'])
-name = df3["libelleVoieEtablissement"]
+# center on Aix 
+m = folium.Map(location=[45.68422679091273, 5.909982061873071], zoom_start=7)
 
 
 
-
-map = folium.Map(location=[46.227638, 2.213749], zoom_start=7, control_scale=True)
-
-tile2 = folium.TileLayer(
-        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        attr = 'Esri',
-        name = 'Esri Satellite',
+# add Google Tile
+tile = folium.TileLayer(
+        tiles = 'http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga',
+        attr = 'Google',
+        name = 'Google Hybrid',
         overlay = False,
         control = True
-       ).add_to(map)
+       ).add_to(m)
 
 
-# for index, location_info in df4.iterrows():
-#     folium.Marker([df4["lat"], df4["lon"]], popup=location_info["libelleVoieEtablissement"]).add_to(map)
-    
-    
-for index,lat in enumerate(latitude):
-    folium.Marker([lat,longitude[index]],popup=name).add_to(map)
-     
+marker_cluster = folium.plugins.MarkerCluster().add_to(m)
 
-st_data = st_folium(map,width = 725)
+for index, location_info in bat_location.iterrows():
+    folium.Marker([location_info["lat"], location_info["lon"]], popup=[location_info["entreprise"]]).add_to(marker_cluster)
 
+# call to render Folium map in Streamlit
+st_map = st_folium(m, width = 725)
 
-# st.pydeck_chart(pdk.Deck(
-#      map_style='mapbox://styles/mapbox/satellite-v9',
-#      initial_view_state=pdk.ViewState(
-#          latitude=46.227638,
-#          longitude=2.213749,
-#          zoom=9,
-#          pitch=50,
-#      ),
-#      layers=[
-#          pdk.Layer(
-#             'HexagonLayer',
-#             data=df3,
-#             get_position='[lon, lat]',
-#             radius=200,
-#             elevation_scale=4,
-#             elevation_range=[0, 1000],
-#             pickable=True,
-#             extruded=True,
-#          ),
-#          pdk.Layer(
-#              'ScatterplotLayer',
-#              data=df,
-#              get_position='[lon, lat]',
-#              get_color='[200, 30, 0, 160]',
-#              get_radius=200,
-#          ),
-#      ],
-#  ))
